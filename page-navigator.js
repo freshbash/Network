9205a7374924c5bae55f8a3b90e50458b5ba802a
Bhashwar <bhaswarsengupta@gmail.com>
2023-07-21 06:59:08 +0530

    initial implementation of page navigator complete

--- page-navigator.js
+++ page-navigator.js
@@ -1,16 +1,24 @@
 //Component to represent page navigator
-
 const PageNavigator = (props) => {
+
+    //Compute the number of tabs the navigator will display
+    const tabs = []
+    for (let i = 1; i <= props.numPages; i++) {        
+        tabs.push(
+            <li class={props.pageNum === i ? "page-item active" : "page-item"}><a class="page-link" href="#">i</a></li>
+        )
+    }
+
     return (
         <div>
-            <nav class="pgn" aria-label="Page navigation example">
-                <ul class="pagination justify-content-center">
-                    <li id="prev" class="page-item disabled">
-                        <a id="link-prev" href="" class="page-link" tabindex="-1" aria-disabled="true">Previous</a>
-                    </li>            
-                    <li id="item-{{ page }}" class="page-item"><a id="link-{{ page }}" class="page-link" href="{% url 'nthpage' 'all' page %}">{{ page }}</a></li>
-                    <li id="next" class="page-item">
-                            <a id="link-next" href="" class="page-link">Next</a>
+            <nav>
+                <ul class="pagination">
+                    <li class={props.pageNum === 1 ? "page-item disabled" : "page-item"}>
+                        <a class="page-link" href="#">Previous</a>
+                    </li>
+                    {tabs}
+                    <li class={props.pageNum === props.numPages ? "page-item disabled" : "page-item"}>
+                        <a class="page-link" href="#">Next</a>
                     </li>
                 </ul>
             </nav>
@@ -18,4 +26,5 @@ const PageNavigator = (props) => {
     )
 }
 
+//Export the component
 export default PageNavigator;
