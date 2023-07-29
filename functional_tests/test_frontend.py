f34049142164cc10601e11549c38326f83fcf4e8
Bhashwar <bhaswarsengupta@gmail.com>
2023-07-29 18:51:34 +0530

    had to register user, to complete the test

--- functional_tests/test_frontend.py
+++ functional_tests/test_frontend.py
@@ -9,27 +9,27 @@ from network.models import User, Post, Follower, Like
 
 class TestNetworkWebsite(StaticLiveServerTestCase):
 
+    #Set up code
     def setUp(self):
         #Set up chrome
         self.browser = webdriver.Chrome()
+
+        self.client = Client()
         
         #Create test users
         self.test_user_1  = User.objects.create(
             username="abc",
             email="a@b.com",
-            password="123",
+            password=123,
             bio="test_user_1 bio"
         )
 
         self.test_user_2  = User.objects.create(
             username="def",
             email="c@d.com",
-            password="456",
+            password=456,
             bio="test_user_2 bio"
         )
-
-        #Log test_user_1 in
-        self.client.force_login(self.test_user_1)
 
         #Create 20 posts, 10 each for both users created
         for i in range(1, 11):
@@ -37,19 +37,49 @@ class TestNetworkWebsite(StaticLiveServerTestCase):
         for i in range(11, 21):
             Post.objects.create(user=self.test_user_2, post="Post number: "+str(i)+" by user: "+self.test_user_2.username)
 
-    def tearDown(self):
-        self.browser.close()
-
-    def test_all_posts_page(self):
-        
         #Make a get request to the appropriate page
         self.browser.get(self.live_server_url)
 
-        #Wait for 5 seconds
-        time.sleep(5)
 
-        #Click the  all posts link
-        self.browser.find_element(By.ID, "all-posts")
+    #Close the browser
+    def tearDown(self):
+        self.browser.close()
+
+
+    #Test the web app
+    def test_all_posts_page(self):
+
+        #Wait for 5 seconds
+        time.sleep(2)
+
+        #Log test_user_1 in
+
+        #Click on log in
+        self.browser.find_element(By.ID, "register").click()
+
+        #Wait for 5 seconds
+        time.sleep(2)
+
+        #Fill in the username
+        self.browser.find_element(By.ID, "username").send_keys("ghi")
+
+        #Fill in the email
+        self.browser.find_element(By.ID, "email").send_keys("e@f.com")
+
+        #Fill in the bio
+        self.browser.find_element(By.ID, "about").send_keys("test_user_3 bio")
+
+        #Fill in the password
+        self.browser.find_element(By.ID, "password").send_keys(789)
+
+        #Fill in the confirmation
+        self.browser.find_element(By.ID, "confirmation").send_keys(789)
+
+        #Click the submit button
+        self.browser.find_element(By.ID, "register-user").click()
+
+        #Wait for 5 seconds
+        time.sleep(2)
 
         #Scroll to the bottom of the page
         self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -59,6 +89,17 @@ class TestNetworkWebsite(StaticLiveServerTestCase):
 
         #Scroll to the top
         self.browser.execute_script("window.scrollTo(document.body.scrollHeight, 0);")
+
+        #Wait for 5 seconds
+        time.sleep(2)
+
+        #Click the create post link
+        self.browser.find_element(By.ID, "create").click()
+
+        #Wait for 2 seconds
+        time.sleep(2)
+
+        #Fill in the post content
 
         #Wait for 5 seconds
         time.sleep(5)
