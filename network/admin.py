718a53b0345fc476af44c3770ffde18467f1ae72
Bhashwar <bhaswarsengupta@gmail.com>
2023-07-31 06:37:20 +0530

    created data for presentation, updated user admin status

--- network/admin.py
+++ network/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from .models import User, Post, Like, Follower
 
+class CustomUserAdmin(UserAdmin):
+    pass
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Post)
 admin.site.register(Like)
 admin.site.register(Follower)
